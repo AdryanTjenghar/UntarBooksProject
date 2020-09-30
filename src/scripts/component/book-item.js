6a552d8b36273e4ab2699f009c8256d55e75ca8b
wikun27-netizen <70614018+wikun27-netizen@users.noreply.github.com>
2020-09-30 11:46:18 +0700

    Add files via upload
    Dari yoshida menambah author dan published date, juga mengedit sedikit navbar dan card.
    Kesulitan menambahkan page untuk menampilkan isi buku(tidak bisa hanya dengan metode [href="books.html"]).
    Awalnya ingin memasukkan description buku, tapi deskripsi tiap buku terlalu acak ada yang kosong; ada yang panjang sekali, sehingga tidak indah dilihat. 
    -->https://developers.google.com/books/docs/v1/reference/volumes --- untuk liat api. yang bisa digunakan.
    -->\src\scripts\component\book-item.js --- untuk mengubah tampilan daftar buku.
    -->https://www.google.com/search?tbm=bks&q=novel --- untuk lihat tampilan dari google books.

--- src/scripts/component/book-item.js
+++ src/scripts/component/book-item.js
@@ -1,16 +1,46 @@
 class BookItem extends HTMLElement {
   set book (book) {
     this._book = book
+
+    this._img_src = book.volumeInfo.imageLinks.thumbnail
+    this._title = book.volumeInfo.title
+
+    if (book.volumeInfo.authors == undefined)
+      this._author = "Tidak diketahui"
+    else 
+      this._author = book.volumeInfo.authors[0]
+
+    if (book.volumeInfo.publishedDate == undefined) 
+      this._date = "Tidak diketahui"
+    else
+      this._date = book.volumeInfo.publishedDate
+
+    //this._desc = book.volumeInfo.description --unused
+
     this.render()
   }
 
   render () {
     this.innerHTML = `
-        <div class="card" style="width:400px">
-            <div class="card-body bg-secondary text-white border rounded">
-              <img src="${this._book.volumeInfo.imageLinks.thumbnail} class="img-fluid"}
-              <h3>${this._book.volumeInfo.title}</h3>
+        <div class="card">
+          <div class="card-body bg-secondary text-white border rounded">
+            <div class="row">
+              <div class="col">
+                <img src="${this._book.volumeInfo.imageLinks.thumbnail}" class="img-fluid">
+              </div>
+              <div class="col">
+                <h4>${this._title}</h4>
+              </div>
             </div>
+            
+            <div class="row">
+              <h6>${this._author} - ${this._date}</h6>
+            </div>
+            <!-- //unused karena desc beragam besarnya
+            <div class="row">
+              <p>${this._desc}</p>
+            </div>-->
+          </div>
         </div>
         `
   }
